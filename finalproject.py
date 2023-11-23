391e1cf82d2c9e0cdd175d476db89db1698027c4
Prince Bashangezi <86910938+PrinceBashangezi@users.noreply.github.com>
2023-11-22 20:35:19 -0800

    creating the main function
    That is where our defined functions will go

---
+++ finalproject.py
@@ -0,0 +1,14 @@
+"""
+    CS051P: Final Project
+
+    Author: Liam Bayer
+            and Prince Bashangezi
+
+    Date:
+"""
+import matplotlib.pyplot as plt
+
+def main():
+
+if __name__ == '__main__':
+    main()
---
+++ finalproject_tester.py
@@ -0,0 +1,6 @@
+from finalproject import *
+
+def main():
+
+if __name__ == "__main__":
+    main()
